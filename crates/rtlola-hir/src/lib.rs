dbe3fb380f477b4f8cfe200dbcae10d0dc4cc309
Maximilian Schwenger <schwenger@react.uni-saarland.de>
2021-03-25 10:15:09 +0100

    Turn on some warnings.

--- crates/rtlola-hir/src/lib.rs
+++ crates/rtlola-hir/src/lib.rs
@@ -1,3 +1,16 @@
+#![forbid(unused_must_use)] // disallow discarding errors
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
 pub mod hir;
 mod modes;
 mod stdlib;
--- crates/rtlola-reporting/src/lib.rs
+++ crates/rtlola-reporting/src/lib.rs
@@ -1,2 +1,15 @@
+#![forbid(unused_must_use)] // disallow discarding errors
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
 mod reporting;
 pub use reporting::*;
